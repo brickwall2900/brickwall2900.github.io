2f2a46af18a2d3e2f3ae683d527ddbbd43ae7eda
brickwall2900 <brickwall2900@gmail.com>
2025-12-26 20:04:00 +0800

    google search console verification

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <svelte:head>
     <title>ROOT</title> 
+    <meta name="google-site-verification" content="ft2V6Ak_UxJv8bkfeytbSQ3X4Q1NlqIe2ghcMLLT8Ss" />
 </svelte:head>
 <script lang="ts">
     import { asset, assets, base, resolve, resolveRoute } from '$app/paths';

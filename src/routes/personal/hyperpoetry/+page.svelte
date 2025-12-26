b96e5b1874f7cd8e57176ae1ca7ff6faab892c84
brickwall2900 <brickwall2900@gmail.com>
2025-12-26 15:19:36 +0800

    update links to use resolve for routing attempt 2

--- src/routes/personal/hyperpoetry/+page.svelte
+++ src/routes/personal/hyperpoetry/+page.svelte
@@ -1,6 +1,10 @@
+<script>
+    import { asset } from "$app/paths";
+
+</script>
 <svelte:head>
     <title>???</title> 
 </svelte:head>
 
-<iframe src="/_hyperpoetry.html" class="w-full h-screen border-none" title="page">
+<iframe src={asset("/_hyperpoetry.html")} class="w-full h-screen border-none" title="page">
 </iframe>

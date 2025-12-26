5f8752b2bb8df2ebe4953dc0408dc2c5c25b0840
brickwall2900 <brickwall2900@gmail.com>
2025-12-26 15:15:43 +0800

    update links to use resolve for routing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,14 @@
 <svelte:head>
     <title>ROOT</title> 
 </svelte:head>
+<script lang="ts">
+    import { asset, assets, base, resolve, resolveRoute } from '$app/paths';
+</script>
+
 
 
 <h1 class="text-xl font-bold">hi welcome to my site</h1>
 <p>so far i just have a few things here:</p>
 <ul class="list-disc ml-8">
-    <li><a href="/textfuscator">Textfuscator</a></li>
+    <li><a href={resolve("/textfuscator")}>Textfuscator</a></li>
 </ul>
--- src/routes/personal/+page.svelte
+++ src/routes/personal/+page.svelte
@@ -3,14 +3,17 @@
 </svelte:head>
 
 <script lang="ts">
+    import { resolve } from "$app/paths";
+
     let links: [string] = [
         "/personal/hyperpoetry"
     ];
 </script>
 
 <p>Please choose an entry. Note that some of these are PASSWORD PROTECTED.</p>
-<ul class="list-disc">
-    {#each links as link}
-        <a href={link} class="underline text-blue-700 dark:text-blue-300"><p>{link}</p></a>
-    {/each}
+<ul class="list-disc ml-8">
+    <li>UNDER CONSTRUCTION</li>
+    <!-- {#each links as link} -->
+        <!-- <a href={resolve(link)} class="underline text-blue-700 dark:text-blue-300"><p>{link}</p></a> -->
+    <!-- {/each} -->
 </ul>

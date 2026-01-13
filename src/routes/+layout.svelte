1cfa0743c4b04a56fe8bbf5c815bc84c7d07a432
brickwall2900 <brickwall2900@gmail.com>
2026-01-14 07:17:45 +0800

    added icons and iconified dakr mode toggle

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
-    import { onMount } from 'svelte';
     import { browser } from '$app/environment';
+    import { Lightbulb, LightbulbOff } from '@lucide/svelte';
 
 	let { children } = $props();
 	
 	const browserLocalStorage = globalThis.localStorage;
 	let localStorage: any[any] = [];
-	let isDarkModeEnabled = false;
+	let isDarkModeEnabled = $state(false);
 	if (browser) {
 		localStorage = browserLocalStorage;
 		isDarkModeEnabled = localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches);
@@ -28,6 +28,15 @@
 	<title>brickwall2900</title>
 </svelte:head>
 
-<button onclick={() => toggleDarkMode()} class="bg-amber-400 hover:bg-amber-500 active:bg-amber-700 text-black dark:text-black fixed right-0 top-0">Toggle dark mode</button>
+<button 
+	onclick={() => toggleDarkMode()} 
+	title="Toggle Dark Mode" 
+	class="bg-amber-400 hover:bg-amber-500 active:bg-amber-700 text-black dark:text-black fixed right-0 top-0 p-2">
+	{#if isDarkModeEnabled}
+		<Lightbulb class="w-full h-full" />
+	{:else}
+		<LightbulbOff class="w-full h-full" />
+	{/if}
+</button>
 
 {@render children()}

672637cad8df253fe5ef456c38c46ffa92bde575
brickwall2900 <brickwall2900@gmail.com>
2026-01-16 19:34:59 +0800

    add other props to image viewer

--- src/lib/components/ImageViewer.svelte
+++ src/lib/components/ImageViewer.svelte
@@ -10,7 +10,7 @@
         <p class="absolute w-full text-center text-white p-2">boo! 👻</p>
         <p class="absolute w-full top-1 text-center text-white mix-blend-exclusion p-2 z-20">Hello!! You're in an image viewer. Drag to pan the image around, pinch or mouse wheel to scroll.</p>
         <Viewer>
-            <enhanced:img src={src} alt={alt} class="rounded shadow-lg max-w-full max-h-full z-10" />
+            <enhanced:img src={src} alt={alt} class="rounded shadow-lg max-w-full max-h-full z-10" {...others} />
         </Viewer>
         <button class="absolute top-2 right-2 m-2 text-white bg-black border border-white opacity-60 rounded-full px-4 py-3 hover:opacity-70 z-30" onclick={closePopup} >
             Close

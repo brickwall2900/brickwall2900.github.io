5e551a3040bdec8ab910dcf6f269872613ae8419
brickwall2900 <brickwall2900@gmail.com>
2026-01-17 09:17:13 +0800

    added dropdown component

---
+++ src/lib/components/Dropdown.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    let { title, children } = $props();
+</script>
+
+<details>
+    <summary class="font-semibold">{title}</summary>
+    <div class="ml-8">
+        {@render children?.()}
+    </div>
+</details>

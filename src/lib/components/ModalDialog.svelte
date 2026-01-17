508b574003469de93ee4ca56ce4b85af43c2e803
brickwall2900 <brickwall2900@gmail.com>
2026-01-17 16:38:51 +0800

    added title bar and close button

--- src/lib/components/ModalDialog.svelte
+++ src/lib/components/ModalDialog.svelte
@@ -1,11 +1,19 @@
 <script lang="ts">
-    let { closeDialog, children } = $props();
+    let { closeDialog, title = "Dialog", children } = $props();
 </script>
 
 <dialog 
     class="fixed w-full h-full mx-auto my-auto inset-0 bg-black/70 flex items-center justify-center z-50"
     onclick={closeDialog}>
     <section class="rounded bg-white dark:bg-gray-950 text-black dark:text-white">
+        <nav class="w-full bg-gray-300 dark:bg-gray-600 px-2 py-1 rounded-tl rounded-tr flex flex-row justify-between">
+            <p class="font-bold text-lg">{title}</p>
+            <button 
+                class="rounded-full bg-red-500 hover:bg-red-600 active:bg-red-700 text-white px-2"
+                onclick={closeDialog}>
+                Close
+            </button>
+        </nav>
         {@render children?.()}
     </section>
 </dialog>

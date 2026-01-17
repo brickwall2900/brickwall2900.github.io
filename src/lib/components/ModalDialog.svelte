92fcf78e3e95485102751aecfcc2e781026629b4
brickwall2900 <brickwall2900@gmail.com>
2026-01-17 16:20:19 +0800

    remove margin on dialog

--- src/lib/components/ModalDialog.svelte
+++ src/lib/components/ModalDialog.svelte
@@ -5,7 +5,7 @@
 <dialog 
     class="fixed w-full h-full mx-auto my-auto inset-0 bg-black/70 flex items-center justify-center z-50"
     onclick={closeDialog}>
-    <section class="rounded bg-white dark:bg-gray-950 text-black dark:text-white p-2">
+    <section class="rounded bg-white dark:bg-gray-950 text-black dark:text-white">
         {@render children?.()}
     </section>
 </dialog>

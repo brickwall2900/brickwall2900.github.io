4de202e8f450a3bdcdbefafa87c6e5165afd9dc5
brickwall2900 <brickwall2900@gmail.com>
2026-01-17 13:00:21 +0800

    reduce margin-top on link list

--- src/lib/components/LinkList.svelte
+++ src/lib/components/LinkList.svelte
@@ -15,7 +15,7 @@
 </script>
 
 {#if links.length > 0}
-    <div class="flex flex-row gap-2 mt-4 items-center">
+    <div class="flex flex-row gap-2 mt-2 items-center">
         <p class="inline mr-2">Links:</p>
         {#each links as link}
             <a href={link} class="bg-gray-300 hover:bg-gray-400 dark:bg-gray-600 dark:hover:bg-gray-700 rounded-2xl px-2 py-1">

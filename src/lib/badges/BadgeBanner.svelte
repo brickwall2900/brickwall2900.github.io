2f32257ac5a7d74cd327a142192c022e5ef00c2b
brickwall2900 <brickwall2900@gmail.com>
2026-01-17 17:18:24 +0800

    disable pointer events on badge icon

--- src/lib/badges/BadgeBanner.svelte
+++ src/lib/badges/BadgeBanner.svelte
@@ -39,7 +39,7 @@
             animate:flip={{ duration: 300 }}>
             <div class="flex flex-row gap-4">
                 <enhanced:img 
-                    class="relative h-12" 
+                    class="relative h-12 pointer-events-none" 
                     src={asset(BADGE_ASSET_URL + notif.id + ".png")} 
                     alt="Badge"/>
                 <div>

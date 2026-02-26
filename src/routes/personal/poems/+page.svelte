5b786ac50e4e35d10c41e8b7e2793a51766bbeb2
brickwall2900 <brickwall2900@gmail.com>
2026-02-26 16:32:50 +0800

    add poetry page

---
+++ src/routes/personal/poems/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import Content from '$lib/components/Content.svelte';
+    import { resolve } from "$app/paths";
+    import type { PageProps } from './$types';
+    import Link from '$lib/components/Link.svelte';
+
+    let { data }: PageProps = $props();
+</script>
+
+{#snippet poemEntry(name: string, url: string)}
+    <div class="my-2">
+        <hr>
+        <Link className="my-2 w-full text-center block" href={resolve(url)}>{name}</Link>
+        <hr>
+    </div>
+{/snippet}
+
+<Content title="Poetry">
+    <p>Did you know? I'm not that much of a writer. However, I like to let my emotions out and feelings sometimes... so I try to write poetry!</p>
+
+    <section>
+        {@render poemEntry("🔒 her guiding light", "/personal/hyperpoetry")}
+        {@render poemEntry("🌹 flower picking", "/personal/poems/flower-picking")}
+    </section>
+</Content>
---
+++ src/routes/personal/poems/flower-picking/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+    import Content from '$lib/components/Content.svelte';
+    import type { PageProps } from './$types';
+
+    let { data }: PageProps = $props();
+</script>
+
+<Content title="flower picking">
+    <p class="whitespace-pre-line text-center">
+        i love the moments when you pick the flowers for me,
+        you're so comfortable telling me why you picked your flowers,
+        you were willing to make time with me picking flowers together,
+        I really enjoyed it, though you put more effort into it however.
+
+        you and I, watched the stars gaze,
+        sometimes I wish, we could do for days,
+        I imagine, how cute and sweet—having someone like you,
+        I never knew my flowers for you, were already blooming and grown.
+
+        more than ever, I wanted to pick flowers,
+        I pushed myself too hard, searching for hours just to find one,
+        even then, you'd sometimes still make time,
+        only then did I realize my mistakes, as I slowly began to wither.
+
+        I started losing hope, thinking I could never keep up,
+        picking up flowers, was never something I was good at,
+        for you, I was never your cup of tea,
+        nor the flowers i hope you quietly grew for me,
+        so I stood quiet, quietly hoping, you never see the flowers I kept behind me.
+    </p>
+</Content>

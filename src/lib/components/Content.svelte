07cb947026de6d930c14cc3a46655f5f5a41ddba
brickwall2900 <brickwall2900@gmail.com>
2026-02-26 16:31:00 +0800

    add className to link and content

--- src/lib/components/Content.svelte
+++ src/lib/components/Content.svelte
@@ -2,11 +2,12 @@
     let {
         title = undefined,
         description = undefined,
+        className = "",
         children
     } = $props();
 </script>
 
-<section class="flex flex-col bg-gray-200 dark:bg-gray-800 m-4 p-4 gap-2 max-w-7xl mx-auto">
+<section class={"flex flex-col bg-gray-200 dark:bg-gray-800 m-4 p-4 gap-2 max-w-7xl mx-auto " + className}>
     {#if title !== undefined}
         <h2 class="text-4xl self-center font-bold w-full text-center mb-1">{title}</h2>
     {/if}

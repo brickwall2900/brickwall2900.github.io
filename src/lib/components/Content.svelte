31ca0731863de594f6d48be2f50df1c57bf31585
brickwall2900 <brickwall2900@gmail.com>
2026-03-23 20:22:50 +0800

    made dark mode more black

--- src/lib/components/Content.svelte
+++ src/lib/components/Content.svelte
@@ -7,7 +7,7 @@
     } = $props();
 </script>
 
-<section class={"flex flex-col bg-gray-200 dark:bg-gray-800 m-4 p-4 gap-2 max-w-7xl mx-auto " + className}>
+<section class={"flex flex-col bg-gray-200 dark:bg-gray-900 m-4 p-4 gap-2 max-w-7xl mx-auto " + className}>
     {#if title !== undefined}
         <h2 class="text-4xl self-center font-bold w-full text-center mb-1">{title}</h2>
     {/if}

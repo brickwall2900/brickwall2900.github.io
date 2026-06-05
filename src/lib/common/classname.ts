5eeb104f1341c1ac8ecb40f94a091590f9ec27c1
brickwall2900 <brickwall2900@gmail.com>
2026-06-05 20:43:03 +0800

    remove tailwind merge logging

--- src/lib/common/classname.ts
+++ src/lib/common/classname.ts
@@ -2,6 +2,5 @@ import { twMerge } from "tailwind-merge";
 
 export function appendClassname(cn1: string, cn2?: string): string {
     const result = !cn2 ? cn1 : twMerge(cn1, cn2);
-    console.log("tailwind merge: %s + %s = %s", cn1, cn2, result);
     return result;
 }

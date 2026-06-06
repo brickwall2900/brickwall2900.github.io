535d4b7c5b7f319d2d23febf8d64796ba43e3a81
brickwall2900 <brickwall2900@gmail.com>
2026-06-06 12:11:51 +0800

    add mdsvex

--- svelte.config.js
+++ svelte.config.js
@@ -1,3 +1,4 @@
+import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
@@ -5,19 +6,21 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-
+	preprocess: [vitePreprocess(), mdsvex({ extensions: ['.svx', '.md'] })],
 	kit: {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter(),
+
 		paths: {
 			base: process.env.BASE_PATH || '',
 			assets: process.env.ASSET_PATH || ''
 		}
-	}
+	},
+	extensions: ['.svelte', '.svx', '.md']
 };
 
 export const trailingSlash = 'always';
+
 export default config;

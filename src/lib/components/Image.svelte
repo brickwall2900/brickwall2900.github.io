60de4203bc67116155c364392f67dbac7c47f8ad
brickwall2900 <brickwall2900@gmail.com>
2026-06-12 01:06:46 +0800

    add a caption and figure to image

--- src/lib/components/Image.svelte
+++ src/lib/components/Image.svelte
@@ -5,12 +5,13 @@
     interface Props {
         src: string,
         alt?: string,
+        caption?: string,
         requiresImageViewer?: boolean,
         class?: string
         [key: string]: any
     }
 
-    let { src, alt, requiresImageViewer = false, class: className, ...others }: Props = $props();
+    let { src, alt, requiresImageViewer = false, caption = undefined, class: className, ...others }: Props = $props();
     let isOpen = $state(false);
 
     function onImageClicked() {
@@ -22,7 +23,14 @@
     }
 </script>
 
-<enhanced:img src={src} alt={alt} class={appendClassname(requiresImageViewer ? "cursor-pointer" : "", className)} {...others} onclick={onImageClicked} />
+{#if caption}
+    <figure>
+        <enhanced:img src={src} alt={alt} class={appendClassname(requiresImageViewer ? "cursor-pointer" : "", className)} {...others} onclick={onImageClicked} />
+        <figcaption class="text-sm text-center mt-2">{caption}</figcaption>
+    </figure>
+{:else}
+    <enhanced:img src={src} alt={alt} class={appendClassname(requiresImageViewer ? "cursor-pointer" : "", className)} {...others} onclick={onImageClicked} />
+{/if}
 
 {#if isOpen && requiresImageViewer}
     <ImageViewer src={src} alt={alt} closePopup={closeTheDamnPopup} {...others} />

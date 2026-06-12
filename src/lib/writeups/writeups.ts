b49ce99d51e3c797982e8e05b0f23aa1994e0437
brickwall2900 <brickwall2900@gmail.com>
2026-06-13 01:43:40 +0800

    move writeups and add data driven writeups

---
+++ src/lib/writeups/writeups.ts
@@ -0,0 +1,77 @@
+import { BlobWriter, ZipWriter } from "@zip.js/zip.js";
+import { uint8ArrayToBase64 } from "../common/base64Helper";
+import writeupList from "$lib/writeups/writeups.json";
+
+const WRITEUP_LIST_LOCATION = "";
+
+export interface WriteupInfo {
+    id: string,
+    name: string,
+    description: string,
+    created: Date,
+    modified?: Date,
+    hasSwearing: boolean,
+}
+
+const WRITEUP_REGISTRY: Map<string, WriteupInfo> = readWriteupData();
+
+function readWriteupData(): Map<string, WriteupInfo> {
+    const registry = new Map<string, WriteupInfo>();
+    writeupList.forEach((value: any) => {
+        registry.set(value.id, {
+            id: value.id,
+            name: value.name,
+            description: value.description,
+            created: new Date(value.created),
+            modified: value.modified ? new Date(value.modified) : undefined,
+            hasSwearing: value.hasSwearing || false
+        });
+    });
+    return registry;
+}
+
+export function getWriteupInfoList(): WriteupInfo[] {
+    return [...WRITEUP_REGISTRY.values()].sort((a, b) => {
+        let firstDate = a.modified ? a.modified : a.created;
+        let secondDate = b.modified ? b.modified : b.created;
+
+        // return firstDate.getTime() - secondDate.getTime();
+        // reverse order vvvvvv
+        return secondDate.getTime() - firstDate.getTime();
+    });
+}
+
+export function getWriteup(id: string): WriteupInfo | undefined {
+    return WRITEUP_REGISTRY.get(id);
+}
+
+export interface WriteupData {
+    name: string,
+    description: string,
+    content: string // base64 zip compressed file
+    coverImage: string | null // base64 png file
+};
+
+export async function generateWriteup(name: string, description: string, content: File[], coverImage: File | null): Promise<WriteupData> {
+    // content compression
+    const contentZipFileWriter = new BlobWriter();
+    const contentZipWriter = new ZipWriter(contentZipFileWriter);
+    for (let i = 0; i < content.length; i++) {
+        const file = content[i];
+        await contentZipWriter.add(file.name, file.stream());
+    }
+    await contentZipWriter.close();
+    const contentBlob = await contentZipFileWriter.getData();
+    const contentBytes = new Uint8Array(await contentBlob.arrayBuffer());
+
+    const coverImageBytes = coverImage ? await coverImage.bytes() : null;
+
+    const data: WriteupData = {
+        name: name,
+        description: description.length > 0 ? description : "< not provided >",
+        content: uint8ArrayToBase64(contentBytes),
+        coverImage: coverImageBytes ? uint8ArrayToBase64(coverImageBytes) : null
+    };
+
+    return data;
+}

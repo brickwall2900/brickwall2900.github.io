63d6a112ff7a2f589b2dead5210c8f4db34b6a77
brickwall2900 <brickwall2900@gmail.com>
2026-09-17 21:12:15 +0800

    add types

---
+++ src/lib/types.svelte.ts
@@ -0,0 +1,134 @@
+export interface Vector2Type {
+    x: number;
+    y: number;
+    getX(): number;
+    getY(): number;
+    set(x: number, y: number): void;
+    setX(x: number): void;
+    setY(y: number): void;
+    scalarDistanceFrom(point: Vector2): number;
+}
+
+export class Vector2 implements Vector2Type {
+    constructor(x: number, y: number) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public getX(): number {
+        return this.x;
+    }
+
+    public getY(): number {
+        return this.y;
+    }
+
+    public set(x: number, y: number) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public setX(x: number) {
+        this.x = x;
+    }
+
+    public setY(y: number) {
+        this.y = y;
+    }
+
+    public scalarDistanceFrom(point: Vector2): number {
+        return Math.sqrt(((point.x - this.x)*(point.x - this.x)) + ((point.y - this.y)*(point.y - this.y)));
+    }
+
+    x: number;
+    y: number;
+}
+
+export class ReactiveVector2 implements Vector2Type {
+    constructor(x: number, y: number) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public getX(): number {
+        return this.x;
+    }
+
+    public getY(): number {
+        return this.y;
+    }
+
+    public set(x: number, y: number) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public setX(x: number) {
+        this.x = x;
+    }
+
+    public setY(y: number) {
+        this.y = y;
+    }
+
+    public scalarDistanceFrom(point: Vector2): number {
+        return Math.sqrt(((point.x - this.x)*(point.x - this.x)) + ((point.y - this.y)*(point.y - this.y)));
+    }
+
+    x: number = $state(0);
+    y: number = $state(0);
+}
+
+export class Rectangle {
+    constructor(top: number, bottom: number, left: number, right: number) {
+        this.#top = top;
+        this.#bottom = bottom;
+        this.#left = left;
+        this.#right = right;
+    }
+
+    public static ofBounds(top: number, bottom: number, left: number, right: number): Rectangle {
+        return new Rectangle(top, bottom, left, right);
+    }
+
+    public static ofWidthHeight(x: number, y: number, width: number, height: number) {
+        return new Rectangle(y, y + height, x, x + width);
+    }
+
+    public getTop(): number {
+        return this.#top;
+    }
+
+    public getBottom(): number {
+        return this.#bottom;
+    }
+
+    public getLeft(): number {
+        return this.#left;
+    }
+
+    public getRight(): number {
+        return this.#right;
+    }
+
+    public getX(): number {
+        return this.getLeft();
+    }
+
+    public getY(): number {
+        return this.getTop();
+    }
+
+    public getWidth(): number {
+        return this.getRight() - this.getLeft();
+    }
+
+    public getHeight(): number {
+        return this.getBottom() - this.getTop();
+    }
+
+    #top: number;
+    #bottom: number;
+    #left: number;
+    #right: number;
+}

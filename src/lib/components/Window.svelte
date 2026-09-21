8f5f894c7b22ce37ee0c723b6762cbb6e9b27e7e
brickwall2900 <brickwall2900@gmail.com>
2026-09-21 22:47:11 +0800

    block windows from exiting the four corners

--- src/lib/components/Window.svelte
+++ src/lib/components/Window.svelte
@@ -2,6 +2,8 @@
     import { fade, fly, slide } from "svelte/transition";
     import Button from "./Button.svelte";
     import type { Snippet } from "svelte";
+    import { MathHelper } from "$lib/common/mathCommon";
+    import { Rectangle, TypesHelper, type RectangleType } from "$lib/types.svelte";
 
     export interface Props {
         onwindowclosing?: (() => boolean),
@@ -10,8 +12,9 @@
         hasCloseButton?: boolean,
         canMove?: boolean,
         canDrag?: boolean,
-        initialX?: number
+        initialX?: number,
         initialY?: number,
+        globalParent?: boolean,
         children: Snippet
     }
 
@@ -24,8 +27,14 @@
         canDrag = $bindable(true),
         initialX = $bindable(0),
         initialY = $bindable(0),
+        globalParent = $bindable(false),
         children
     }: Props = $props();
+
+    let theWindowItself: HTMLElement | undefined = undefined;
+
+    let viewportBounds = $state<RectangleType | undefined>(undefined);
+    let windowBounds = $state<RectangleType | undefined>(undefined);
 
     let titlebarPressed = $state(false);
     let offsetX = $state(initialX), offsetY = $state(initialY);
@@ -64,8 +73,17 @@
         }
 
         if (titlebarPressed) {
-            offsetX += e.movementX;
-            offsetY += e.movementY;
+            const castedWindowElement = (theWindowItself as HTMLElement | undefined);
+            const theParent = castedWindowElement?.parentElement;
+            if (castedWindowElement && theParent) {
+                console.log(theParent);
+                viewportBounds = !globalParent ? TypesHelper.domRectToRectangle(theParent.getBoundingClientRect()) : Rectangle.ofWidthHeight(0, 0, window.innerWidth, window.innerHeight);
+                windowBounds = TypesHelper.domRectToRectangle(castedWindowElement.getBoundingClientRect());
+                console.log(viewportBounds, windowBounds);
+
+                offsetX += MathHelper.clamp(windowBounds.left + e.movementX, viewportBounds.left, viewportBounds.right - windowBounds.getWidth()) - windowBounds.left;
+                offsetY += MathHelper.clamp(windowBounds.top + e.movementY, viewportBounds.top, viewportBounds.bottom - windowBounds.getHeight()) - windowBounds.top;
+            }
             e.preventDefault();
         }
     }
@@ -84,7 +102,6 @@
         if (titlebarPressed) {
             offsetX = touch.clientX - touchOffsetX;
             offsetY = touch.clientY - touchOffsetY;
-            
         }
     }
 
@@ -93,11 +110,21 @@
             e.preventDefault();
         }
     }
+
+    function onResized(e: Event) {
+        //windowBounds = (theWindowItself as HTMLElement | undefined)?.getBoundingClientRect();
+    }
 </script>
 
-<svelte:window onpointermove={onMouseMoved} ontouchmove={onTouchDragged} onscroll={onScroll} onpointerup={onMouseReleased} />
+<svelte:window 
+    onpointermove={onMouseMoved} 
+    ontouchmove={onTouchDragged} 
+    onscroll={onScroll} 
+    onpointerup={onMouseReleased}
+    onresize={onResized} />
 
 <section 
+    bind:this={theWindowItself}
     class={"w-fit fixed bg-window text-content-text border-4 border-button-border"}
     in:fly={{duration: 250, y: -100}}
     out:fly={{duration: 250, y: 100}}
